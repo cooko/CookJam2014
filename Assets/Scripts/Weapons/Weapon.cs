f1e048956d8942244910d76241537674a408971a
Simon Cook <simon@sandglaz.com>
2014-08-03 11:24:02 -0400

    2 kinds of shoot. Architecture.

--- Assets/Scripts/Weapons/Weapon.cs
+++ Assets/Scripts/Weapons/Weapon.cs
@@ -2,43 +2,48 @@
 using System.Collections;
 
 public class Weapon : MonoBehaviour {
-	public GameObject Projectile;
-	public float FireRate;
-	public float knockback;
-	protected float cooldown;
-	protected bool fireable;
+	public GameObject projectilePrimary;
+	public GameObject projectileSecondary;
+	public GameObject turretPrimary;
+	public GameObject turretSecondary;
+	public float fireRatePrimary;
+	public float fireRateSecondary;
+	public float fireRateTurret;
+	protected float cooldownPrimary = 0;
+	protected float cooldownSecondary = 0;
+	protected float cooldownTurret = 0;
 
 	// Use this for initialization
 	void Start () {
-		fireable = true;
-		cooldown = 0;
-		FireRate = 1/FireRate;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		bool fire = Input.GetButton("Fire1");
-
-		if(cooldown > 0){
-			cooldown -= 1 * Time.deltaTime;
-		}else{
-			if(fire){
-				Fire();
-				cooldown = FireRate; 
-			}			
+		print(cooldownPrimary);
+		if (Input.GetAxis("Fire2") > 0 && cooldownSecondary < 0) {
+			Fire(projectileSecondary);
+			cooldownSecondary = fireRateSecondary;
 		}
-	}
-	public bool Fire(){
-		//if(fireable){
-			GameObject projectile = (GameObject) Instantiate (Projectile, transform.position, transform.rotation);
-			//Projectile projectile_script = projectile.GetComponent<Projectile>();
-			//projectile_script.player = transform.parent.gameObject.name;
-			//fireable = false;
-			//cooldown = FireRate;
-			return true;
-		//}else{
-		//	return false;
-		//}
+		else if (Input.GetButton("Fire1") && cooldownPrimary < 0) {
+			Fire(projectilePrimary);
+			cooldownPrimary = fireRatePrimary;
+		}
 
+		if (cooldownTurret < 0) {
+			if (Input.GetButton("Fire3")) {
+				Fire (turretPrimary);
+			}
+			else if (Input.GetAxis("Fire4") > 0) {
+				Fire (turretSecondary);
+			}
+		}
+
+		cooldownPrimary -= 1 * Time.deltaTime;
+		cooldownSecondary -= 1 * Time.deltaTime;
+		cooldownTurret -= 1 * Time.deltaTime;
+	}
+
+	public void Fire(GameObject projectile) {
+		Instantiate (projectile, transform.position, transform.rotation);
 	}
 }
